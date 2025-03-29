d83df3738d5257f4bd738c1b500f43c96614caff
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2025-03-29 23:06:32 +0100

    Create dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 
-export default function Home() {
+export default async function Home() {
+  // const movies = await fetch();
+  // const data = await movies.json();
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
 
