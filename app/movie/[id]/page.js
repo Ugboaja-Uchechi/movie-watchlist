efb9cdabc66c9e144bcd576e0c61eabcee21eadb
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2025-04-06 17:45:06 +0100

    Fix bugs and improve code

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,34 +1,52 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState, use } from 'react'
 import axios from 'axios';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-export default async function MovieDetail({ params }) {
-  const { id } = params;
-  const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+export default function MovieDetail({ params }) {
+  const { id } = use(params);
+  const [movieDetail, setMovieDetail] = useState(null);
+  const router = useRouter();
 
-  const movieResponse = await axios.get(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
-  );
-  const movie = movieResponse.data;
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+        const movieResponse  = await axios.get(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+        );
+        const movie = movieResponse.data;
+        setMovieDetail(movie);
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+      }
+    }
+    fetchMovie();
+  }, []);
+
+  const handleWatch = () => {
+    console.log("Navigating to watch:", id);
+    router.push(`/watch/${id}`);
+  };
 
   return (
     <div className="p-4 max-w-2xl mx-auto">
       <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500${movieDetail?.poster_path}`}
         className="w-full max-h-[400px] h-full rounded-lg object-contain mt-4"
       />
       <div className='flex items-center justify-between'>
         <div className='flex-2'>
-          <h1 className="text-2xl font-bold text-center">{movie.title}</h1>
-          <p className="mt-4 text-gray-800">{movie.overview}</p>
-          <p className="mt-2 font-semibold">Rating: {movie.vote_average}</p>
+          <h1 className="text-2xl font-bold text-center">{movieDetail?.title}</h1>
+          <p className="mt-4 text-gray-800">{movieDetail?.overview}</p>
+          <p className="mt-2 font-semibold">Rating: {movieDetail?.vote_average}</p>
         </div>
         <div className="flex-1 text-center">
-          <Link href={`/watch/${id}`}>
-            <button className="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  
+            <button className="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleWatch}>
               Watch Now
             </button>
-          </Link>
         </div>
       </div>
 
@@ -37,5 +55,5 @@ export default async function MovieDetail({ params }) {
       
     </div>
   )
-}
 
+}
