f1f995062f9ceb5b231ce97e48ef93cc1c588841
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2025-04-06 16:59:50 +0100

    Build details layout

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -13,21 +13,28 @@ export default async function MovieDetail({ params }) {
 
   return (
     <div className="p-4 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold text-center">{movie.title}</h1>
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        className="w-full rounded-lg mt-4"
+        className="w-full max-h-[400px] h-full rounded-lg object-contain mt-4"
       />
-      <p className="mt-4 text-gray-800">{movie.overview}</p>
-      <p className="mt-2 font-semibold">Rating: {movie.vote_average}</p>
-
-      <div className="mt-4">
-        <Link href={`/watch/${id}`}>
-          <button className="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Watch Now
-          </button>
-        </Link>
+      <div className='flex items-center justify-between'>
+        <div className='flex-2'>
+          <h1 className="text-2xl font-bold text-center">{movie.title}</h1>
+          <p className="mt-4 text-gray-800">{movie.overview}</p>
+          <p className="mt-2 font-semibold">Rating: {movie.vote_average}</p>
+        </div>
+        <div className="flex-1 text-center">
+          <Link href={`/watch/${id}`}>
+            <button className="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Watch Now
+            </button>
+          </Link>
+        </div>
       </div>
+
+
+
+      
     </div>
   )
 }
